4a58b76f40f8d81bc4b0c8ef26dca8a8f57d2182
ValentinoBessone <vbessone11@gmail.com>
2025-10-20 16:37:27 -0300

    cambios

--- src/app/pages/register-page/register-page.ts
+++ src/app/pages/register-page/register-page.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { UserServices } from '../../services/user-services';
+import { Spinner } from '../../components/spinner/spinner';
+
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [RouterModule, FormsModule,Spinner],
   templateUrl: './register-page.html',
   styleUrl: './register-page.scss'
 })
 export class RegisterPage {
+  userService: UserServices = inject(UserServices);
+  errorRegister= false;
+  isLoading = false;
+  router: Router = inject(Router);
+
+  async register(form:NgForm){
+    this.errorRegister = false;
+    if(!form.value.email ||
+      !form.value.password ||
+      !form.value.password2 ||
+      !form.value.firstName ||
+      !form.value.lastName ||
+      form.value.password !== form.value.password2){
+      this.errorRegister = true;
+      return
+    }
+    
+    this.isLoading = true;
+    const res = await this.userService.register(form.value)
+    if(res.ok){
+      this.router.navigate(["/login"])
+    }
+    this.isLoading = false;
+    this.errorRegister = true
+  }
 
 }
+
